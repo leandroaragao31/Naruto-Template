49a29a3840e1e739de83a40a0703ef273d7c8e33
leandroaragao31 <leandroaragao31@gmail.com>
2021-11-12 14:39:43 -0300

    frist commit , update pluguins

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,44 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Naruto-Template",
   },
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: [
+    "gatsby-plugin-styled-components",
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "alldata",
+        fieldName: "alldata",
+        url: "https://api-us-east-1.graphcms.com/v2/ckvvjfwld2qcb01z045r9b8w0/master",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: 'src/images/naruto.png',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts-v2`,
+      options: {
+        fonts: [
+          {
+            family: 'JetBrains Mono',
+            weights: ['100', '400']
+          },
+          {
+            family: 'Roboto Mono',
+            weights: ['100..400']
+          }
+        ]
+      }
+    }
+  ],
 };
+
